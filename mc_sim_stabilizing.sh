c579a23f8570e4cfcac10e7dae871f0dba67424c
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2020-09-16 10:02:41 -0600

    running scripts for mixed stabilizing and equal comp"

---
+++ mc_sim_stabilizing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --account=coexistence
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nathan.wisnoski@uwyo.edu
+#SBATCH --job-name=metacom_coexistence
+
+cd /home/nwisnosk/GitHub/metacom-coexistence
+
+module load gcc/7.3.0 r/3.6.1
+
+R CMD BATCH --no-restore --no-save --quiet mc_sim.R mc_sim.log
