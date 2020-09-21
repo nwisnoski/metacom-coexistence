dbb8d463e05c35e7304efdc22e2db7b4de977afc
Nathan Wisnoski <wisnoski@indiana.edu>
2020-09-21 12:47:17 -0600

    write plotting functions

---
+++ plot_diversity.R
@@ -0,0 +1,97 @@
+# analyze diversity
+library("tidyverse")
+library("data.table")
+
+plot_diversity <- function(dynamics_total, file){
+  
+  # plot aboveground
+  last_t_out <- dynamics_total %>%
+    filter(time == max(dynamics_total$time),
+           N > 0) %>%
+    group_by(dispersal, germination, survival, patch) %>%
+    arrange(patch)
+  
+  alpha_out <- last_t_out %>%
+    mutate(N_pa = 1*(N > 0)) %>%
+    summarize(alpha = sum(N_pa)) %>%
+    ungroup()
+  
+  alpha_out <- alpha_out %>%
+    group_by(dispersal, germination, survival) %>%
+    summarize(mean_alpha = mean(alpha))
+  
+  gamma_out <- last_t_out %>%
+    ungroup() %>%
+    mutate(N_pa = 1*(N > 0)) %>%
+    group_by(dispersal, germination, survival) %>%
+    summarize(gamma = length(unique(species)))
+  
+  div_part <- alpha_out %>%
+    left_join(gamma_out) %>%
+    mutate(beta = gamma / mean_alpha)
+  
+  div_part %>%
+    mutate(germination = factor(germination, levels = germ_fracs, labels = paste("Germ. =", round(germ_fracs,2))),
+           survival = factor(survival, levels = surv_fracs, labels = paste("Surv. =", round(surv_fracs,2)))) %>%
+    gather(mean_alpha, beta, gamma, key = "partition", value = "diversity") %>%
+    mutate(partition = factor(partition, levels = c("mean_alpha", "beta", "gamma"))) %>%
+    ggplot(aes(x = dispersal, y = diversity, color = partition, fill = partition)) +
+    geom_point(alpha = 0.5) +
+    geom_smooth() +
+    facet_grid(rows = vars(germination), cols = vars(survival), drop = FALSE) +
+    theme_light() +
+    scale_x_log10() +
+    theme(legend.position = "top") +
+    ggsave(paste0("figures/diversity_partitioning_",str_remove(file,".pdf"),"_above.pdf"), width = 8, height = 6)
+  
+  # plot seedbank
+  last_t_out <- dynamics_total %>%
+    filter(time == max(dynamics_total$time),
+           D > 0) %>%
+    group_by(dispersal, germination, survival, patch) %>%
+    arrange(patch)
+  
+  alpha_out <- last_t_out %>%
+    mutate(D_pa = 1*(D > 0)) %>%
+    summarize(alpha = sum(D_pa)) %>%
+    ungroup()
+  
+  alpha_out <- alpha_out %>%
+    group_by(dispersal, germination, survival) %>%
+    summarize(mean_alpha = mean(alpha))
+  
+  gamma_out <- last_t_out %>%
+    ungroup() %>%
+    mutate(D_pa = 1*(D > 0)) %>%
+    group_by(dispersal, germination, survival) %>%
+    summarize(gamma = length(unique(species)))
+  
+  div_part <- alpha_out %>%
+    left_join(gamma_out) %>%
+    mutate(beta = gamma / mean_alpha)
+  
+  div_part %>%
+    mutate(germination = factor(germination, levels = germ_fracs, labels = paste("Germ. =", round(germ_fracs,2))),
+           survival = factor(survival, levels = surv_fracs, labels = paste("Surv. =", round(surv_fracs,2)))) %>%
+    gather(mean_alpha, beta, gamma, key = "partition", value = "diversity") %>%
+    mutate(partition = factor(partition, levels = c("mean_alpha", "beta", "gamma"))) %>%
+    ggplot(aes(x = dispersal, y = diversity, color = partition, fill = partition)) +
+    geom_point(alpha = 0.5) +
+    geom_smooth() +
+    facet_grid(rows = vars(germination), cols = vars(survival), drop = FALSE) +
+    theme_light() +
+    scale_x_log10() +
+    theme(legend.position = "top") +
+    ggsave(paste0("figures/diversity_partitioning_",str_remove(file,".pdf"),"_below.pdf"), width = 8, height = 6)
+  
+}
+
+
+files <- list.files("sim_output/")
+for(f in files){
+  dat <- data.table::fread(f)
+  plot_diversity(dat, file = f)
+}
+
+
+
---
+++ plot_diversity.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --account=coexistence
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nathan.wisnoski@uwyo.edu
+#SBATCH --job-name=plot_diversity
+
+cd /project/coexistence/nwisnosk/GitHub/metacom-coexistence
+
+module load gcc/7.3.0 r/3.6.1
+
+R CMD BATCH --no-restore --no-save  plot_diversity.R plot_diversity.log
