7ec4b6513b3125a84c1e604fdac624fe2354221c
Nathan Wisnoski <wisnoski@indiana.edu>
2020-10-20 16:07:27 -0600

    make single script to run both competition conditions

--- mc_sim.sh
+++ mc_sim.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -l
 
 #SBATCH --account=coexistence
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --cpus-per-task=1
+#SBATCH --mem=96GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nathan.wisnoski@uwyo.edu
-#SBATCH --job-name=metacom_coexistence
+#SBATCH --job-name=metacom_equal
 
-cd /home/nwisnosk/GitHub/metacom-coexistence
+cd /gscratch/nwisnosk/GitHub/metacom-coexistence
 
 module load gcc/7.3.0 r/3.6.1
 
-R CMD BATCH --no-restore --no-save --quiet mc_sim.R mc_sim.log
+R CMD BATCH --no-restore --no-save mc_sim.R mc_sim.log
