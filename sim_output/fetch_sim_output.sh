bc8ed64b379d7a4a005fa6cea58145f68b86a528
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-06-30 12:44:03 -0600

    added script to fetch updated simulation data from teton

---
+++ sim_output/fetch_sim_output.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ scp nwisnosk@teton.arcc.uwyo.edu:/gscratch/nwisnosk/GitHub/metacom-coexistence/sim_output/final_stable_* .
+ scp nwisnosk@teton.arcc.uwyo.edu:/gscratch/nwisnosk/GitHub/metacom-coexistence/sim_output/final_equal_* .
