1119fa9cf7732943d38328bf15a5e3d1581cad58
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-07-12 11:27:28 -0600

    updated path to mcsim.sh, added tradeoff simulation scenario

--- mc_sim_tradeoff.sh
+++ mc_sim_tradeoff.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=coexistence
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=124GB
 #SBATCH --mail-type=ALL
@@ -14,4 +14,4 @@ cd /gscratch/nwisnosk/GitHub/metacom-coexistence
 
 module load gcc/7.3.0 r/3.6.1
 
-R CMD BATCH --no-restore --no-save mc_sim.R mc_sim.log
+R CMD BATCH --no-restore --no-save mc_sim_tradeoff.R mc_sim_tradeoff.log
