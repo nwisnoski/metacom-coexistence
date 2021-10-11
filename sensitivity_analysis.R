92884ba60adfc4c4867cbcdee941dbc36c233272
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-10-11 16:56:00 -0600

    improved sens figure and made supp figs

--- sensitivity_analysis.R
+++ sensitivity_analysis.R
@@ -16,40 +16,42 @@ theme_set(theme_light() +
                   legend.title = element_text(size = 14),
                   legend.text = element_text(size = 12)))
 
-sim_dat <- read_csv(file = "sim_output/final_sensitivity_2021-09-29_001433.csv")
+# read in raw data. Below, I'm using intermediate output, the code to create this is commented out but can be regenerated.
+# sim_dat <- read_csv(file = "sim_output/final_sensitivity_2021-09-29_001433.csv")
+# 
+# # params <- sim_dat %>% 
+# #   select(rep, dispersal, germination, survival, comp) %>% 
+# #   unique()
+# 
+# nenvs <- 20
+# nreps <- 250
+# nspecies <- 40
+# npatchs <- 100
+# 
+# nenvs * nreps * nspecies * npatchs * 2 == nrow(sim_dat)
+# 
+# # re-assign environment scenario to group by
+# sim_dat$envscenario <- rep(1:nenvs, each = (nreps * nspecies * npatchs * 2))
+# 
+# alpha <- sim_dat %>% 
+#   group_by(patch, envscenario, rep, dispersal, germination, survival, comp) %>% 
+#   summarize(alpha = sum(1*(N>0))) %>%
+#   ungroup() %>% 
+#   group_by(rep, envscenario, comp) %>%
+#   summarize(mean_alpha = mean(alpha))
+# # left_join(params, by = c("rep", "comp"))
+# write_csv(alpha, file = "summarized_output/sens_analysis_alphadiv.csv")
+# 
+# gamma <- sim_dat %>% 
+#   mutate(N_pa = 1*(N > 0)) %>%
+#   group_by(comp, envscenario, rep, dispersal, germination, survival, species) %>%
+#   
+#   summarize(presences = sum(N_pa)) %>% 
+#   summarize(gamma = sum(presences > 0))
+# write_csv(gamma, file = "summarized_output/sens_analysis_gammadiv.csv")
 
-# params <- sim_dat %>% 
-#   select(rep, dispersal, germination, survival, comp) %>% 
-#   unique()
-
-nenvs <- 20
-nreps <- 250
-nspecies <- 40
-npatchs <- 100
-
-nenvs * nreps * nspecies * npatchs * 2 == nrow(sim_dat)
-
-# re-assign environment scenario to group by
-sim_dat$envscenario <- rep(1:nenvs, each = (nreps * nspecies * npatchs * 2))
-
-sim_dat %>% 
-  group_by(patch, envscenario, rep, dispersal, germination, survival, comp) %>% 
-  summarize(alpha = sum(1*(N>0))) %>%
-  ungroup() %>% 
-  group_by(rep, envscenario, comp) %>%
-  summarize(mean_alpha = mean(alpha))
-# left_join(params, by = c("rep", "comp"))
-write_csv(alpha, file = "summarized_output/sens_analysis_alphadiv.csv")
-
-gamma <- sim_dat %>% 
-  mutate(N_pa = 1*(N > 0)) %>%
-  group_by(comp, envscenario, rep, dispersal, germination, survival, species) %>%
-  
-  summarize(presences = sum(N_pa)) %>% 
-  summarize(gamma = sum(presences > 0))
-write_csv(gamma, file = "summarized_output/sens_analysis_gammadiv.csv")
-
-
+alpha <- read_csv("summarized_output/sens_analysis_alphadiv.csv")
+gamma <- read_csv("summarized_output/sens_analysis_gammadiv.csv")
 
 div_part <- alpha %>% 
   left_join(gamma) %>% 
@@ -61,6 +63,7 @@ div_part <- alpha %>%
 div_part$mean_alpha <- normalize(div_part$mean_alpha)
 div_part$beta <- normalize(div_part$beta)
 div_part$gamma <- normalize(div_part$gamma)
+div_part$dispersal <- normalize(log(div_part$dispersal))
 
 # alpha_mod_eq <- lm(mean_alpha ~ normalize(log(dispersal)) * germination * survival, data = subset(div_part, comp == "equal"))
 # summary(alpha_mod_eq)
@@ -84,7 +87,6 @@ div_part$gamma <- normalize(div_part$gamma)
 
 
 # try with quadratic terms
-div_part$dispersal <- normalize(log(div_part$dispersal))
 alpha_mod_eq <- lm(mean_alpha ~ dispersal + germination + survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "equal"))
 summary(alpha_mod_eq)
 
@@ -167,19 +169,114 @@ sensitivity_plot <- sens_out %>%
              color = response, fill = response, x = term)) +
   geom_bar(stat = "identity",
            position = position_dodge(), alpha = 0.7) +
+  geom_vline(xintercept = c(1:6+0.5), alpha = 0.5, color = "gray50", size = 0.2) +
   geom_errorbar(position = position_dodge(.9), width = 0.5, show.legend = FALSE) +
   scale_color_manual(values = darken(my_cols, amount = .2)) + 
   scale_fill_manual(values = my_cols) +
   theme(panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
-        legend.position = c(.08,.9),
+        legend.position = c(.93,.62),
+        legend.background = element_rect(color = "black"),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 11),
         axis.text.x = element_text(margin = margin(t = 10), vjust = 0)) +
   facet_wrap(~comp, nrow = 2) +
   labs(x = "", y = "Effect Size", fill = "Diversity level", color = "Diversity level") +
   scale_x_discrete(labels = x_axis_labs)
 
-#sensitivity_plot
+sensitivity_plot
 
 ggsave(filename = "figures/sensitivity_analysis.pdf",plot = sensitivity_plot, width = 10, height = 8)
 
+############################
+# Incorporate interaction terms
 
+alpha_mod_eq <- lm(mean_alpha ~ dispersal * germination * survival * I(dispersal^2) * I(germination^2) * I(survival^2), data = subset(div_part, comp == "equal"))
+summary(alpha_mod_eq)
+
+beta_mod_eq <- lm(beta ~ dispersal * germination * survival * I(dispersal^2) * I(germination^2) * I(survival^2), data = subset(div_part, comp == "equal"))
+summary(beta_mod_eq)
+
+gamma_mod_eq <- lm(gamma ~ dispersal * germination * survival * I(dispersal^2) * I(germination^2) * I(survival^2), data = subset(div_part, comp == "equal"))
+summary(gamma_mod_eq)
+
+alpha_mod_stab <- lm(mean_alpha ~ dispersal * germination * survival * I(dispersal^2) * I(germination^2) * I(survival^2), data = subset(div_part, comp == "stable"))
+summary(alpha_mod_stab)
+
+beta_mod_stab <- lm(beta ~ dispersal * germination * survival * I(dispersal^2) * I(germination^2) * I(survival^2), data = subset(div_part, comp == "stable"))
+summary(beta_mod_stab)
+
+gamma_mod_stab <- lm(gamma ~ dispersal * germination * survival * I(dispersal^2) * I(germination^2) * I(survival^2), data = subset(div_part, comp == "stable"))
+summary(gamma_mod_stab)
+
+### 
+alpha_mod_eq <- lm(mean_alpha ~ dispersal * germination * survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "equal"))
+summary(alpha_mod_eq)
+
+beta_mod_eq <- lm(beta ~ dispersal * germination * survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "equal"))
+summary(beta_mod_eq)
+
+gamma_mod_eq <- lm(gamma ~ dispersal * germination * survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "equal"))
+summary(gamma_mod_eq)
+
+alpha_mod_stab <- lm(mean_alpha ~ dispersal * germination * survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "stable"))
+summary(alpha_mod_stab)
+
+beta_mod_stab <- lm(beta ~ dispersal * germination * survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "stable"))
+summary(beta_mod_stab)
+
+gamma_mod_stab <- lm(gamma ~ dispersal * germination * survival + I(dispersal^2) + I(germination^2) + I(survival^2), data = subset(div_part, comp == "stable"))
+summary(gamma_mod_stab)
+
+
+# build dataframe to plot
+sens_out <- tidy(alpha_mod_eq) %>% 
+  mutate(response = "alpha", comp = "equal") %>% 
+  bind_rows(
+    (tidy(beta_mod_eq) %>% 
+       mutate(response = "beta", comp = "equal")), 
+    (tidy(gamma_mod_eq) %>% 
+       mutate(response = "gamma", comp = "equal")),
+    
+    (tidy(alpha_mod_stab) %>% 
+       mutate(response = "alpha", comp = "stable")),
+    (tidy(beta_mod_stab) %>% 
+       mutate(response = "beta", comp = "stable")), 
+    (tidy(gamma_mod_stab) %>% 
+       mutate(response = "gamma", comp = "stable"))
+  ) 
+
+my_cols <- qualitative_hcl(n = 3)
+
+
+int_sensitivity_plot <- sens_out %>% 
+  # mutate(term = factor(term, levels = c(
+  #   "Intercept", "dispersal", "germination", "survival",
+  #   "dispersal:germination", "dispersal:survival", "germination:survival",
+  #   "dispersal:germination:survival"
+  # ), labels = c(
+  #   "intercept", "dispersal", "germination", "survival",
+  #   "dispersal x \n germination", "dispersal x \n survival", "germination x \n survival",
+  #   "dispersal x \n germination x \n survival"
+  # ))) %>%
+  
+  ggplot(aes(y = estimate, ymin = (estimate - std.error), ymax = (estimate + std.error),
+             color = response, fill = response, x = term)) +
+  geom_bar(stat = "identity",
+           position = position_dodge(), alpha = 0.7) +
+  geom_errorbar(position = position_dodge(.9), width = 0.5, show.legend = FALSE) +
+  scale_color_manual(values = darken(my_cols, amount = .2)) + 
+  scale_fill_manual(values = my_cols) +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.position = c(.93,.62),
+        legend.background = element_rect(color = "black"),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 11),
+        axis.text.x = element_text(size = 12, angle = 45, hjust = 1)) +
+  facet_wrap(~comp, nrow = 2) +
+  labs(x = "", y = "Effect Size", fill = "Diversity level", color = "Diversity level") #+
+  # coord_flip()
+
+int_sensitivity_plot
+ggsave("figures/sensitivity_analysis_int_quad.pdf", width = 15, height = 10)
