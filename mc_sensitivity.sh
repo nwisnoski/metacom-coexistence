c3e9326354262f68d4806820db5a28998514456a
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-10-11 14:54:22 -0600

    updating with new grouping and intermediate output

---
+++ mc_sensitivity.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --account=coexistence
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nathan.wisnoski@uwyo.edu
+#SBATCH --job-name=metacom
+
+cd /gscratch/nwisnosk/GitHub/metacom-coexistence
+
+module load gcc/7.3.0 r/3.6.1
+
+R CMD BATCH --no-restore --no-save sensitivity_analysis.R sensitivity_analysis.log
