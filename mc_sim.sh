add71782f874adb8bf7cb9fee6b7d30267689626
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-11-10 10:22:05 -0700

    run 0.95 survival

--- mc_sim.sh
+++ mc_sim.sh
@@ -3,15 +3,14 @@
 #SBATCH --account=coexistence
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=124GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nathan.wisnoski@uwyo.edu
-#SBATCH --job-name=metacom
+#SBATCH --job-name=metacom_95
 
 cd /gscratch/nwisnosk/GitHub/metacom-coexistence
 
 module load gcc/7.3.0 r/3.6.1
 
-R CMD BATCH --no-restore --no-save mc_sim.R mc_sim.log
+R CMD BATCH --no-restore --no-save mc_sim_0.95.R mc_sim95.log
